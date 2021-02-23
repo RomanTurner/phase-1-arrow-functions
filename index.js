70ca6e95f372a7ee45aa315334d65b626515132f
Roman Turner <rn.turner@live.com>
2021-02-22 19:49:57 -0800

    commit tree 812983355c14aa826340f66642cefd16f1126e5f

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (num1, num2) => +(num1 / num2);
+console.log(divide(2000, 100));
+
+const square = (num) => num ** 2; 
+console.log(square(2));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(3,4));
